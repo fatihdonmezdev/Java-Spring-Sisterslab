b6dc635cdf942936730f90de352e0b72b43231d9
Fatih <fdonmez_1512@hotmail.com>
2024-01-10 21:33:14 +0300

    Added the project

---
+++ src/odev1/odev1.java
@@ -0,0 +1,19 @@
+package odev1;
+
+import java.util.Scanner;
+
+public class odev1 {
+    public  static  void main(String[] args){
+        double kdv = 0.30;
+        Scanner input = new Scanner(System.in);
+        System.out.println("KDVsini hesaplatmak istediğiniz ürünü giriniz.");
+        double sayi1 = input.nextInt();
+        double kdvMiktari = sayi1 * kdv;
+        System.out.println("Ürünün KDVsiz fiyatı: " +sayi1);
+        System.out.println("Ürünün KDVli fiyatı: " + (sayi1 + kdvMiktari));
+        System.out.println("Ürüne uygulanan KDV miktarı: " + kdvMiktari);
+
+
+    }
+
+}
---
+++ src/odev2/Odev2.java
@@ -0,0 +1,18 @@
+package odev2;
+
+import java.util.Scanner;
+
+public class Odev2 {
+    public  static  void main(String[] args){
+        double kdv = 0.30;
+        Scanner input = new Scanner(System.in);
+        System.out.println("KDVsini hesaplatmak istediğiniz ürünü giriniz.");
+        double sayi1 = input.nextInt();
+        double kdvMiktari = sayi1 * kdv;
+        System.out.println("Ürünün KDVsiz fiyatı: " +sayi1);
+        System.out.println("Ürünün KDVli fiyatı: " + (sayi1 + kdvMiktari));
+        System.out.println("Ürüne uygulanan KDV miktarı: " + kdvMiktari);
+
+
+    }
+}
